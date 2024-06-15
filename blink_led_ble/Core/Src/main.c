a125873179c120c6d0b2d1459e737093eef239e7
Eya CHHOUMI <130108544+Eyachhoumi@users.noreply.github.com>
2024-06-15 15:48:22 +0100

    main.c

--- blink_led_ble/Core/Src/main.c
+++ blink_led_ble/Core/Src/main.c
@@ -18,7 +18,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "Inc/main.h"
+#include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
